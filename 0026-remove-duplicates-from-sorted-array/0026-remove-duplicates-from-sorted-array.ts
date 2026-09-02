d38bd265be3e455ddf7118e3d9ce48e1e416608f
akhilpgeorge <akhilpgeorge@gmail.com>
2026-09-02 21:33:49 +0530

    Time: 56 ms (9.71%), Space: 58.6 MB (64.65%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,6 +1,5 @@
 function removeDuplicates(nums: number[]): number {
     for(let i: number=0; i<nums.length; i++){
-        let duplicateIndex: number = nums.indexOf(nums[i]);
         if (nums.indexOf(nums[i]) !== i) {
             nums.splice(i,1);
             i--;
