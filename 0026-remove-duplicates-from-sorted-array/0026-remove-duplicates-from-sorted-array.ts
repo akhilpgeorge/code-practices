c954ea6b81a522d722627c8bed1c55115904c421
akhilpgeorge <akhilpgeorge@gmail.com>
2026-09-02 21:38:31 +0530

    Time: 0 ms (100%), Space: 58.6 MB (58.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,9 +1,24 @@
+// function removeDuplicates(nums: number[]): number {
+//     for(let i: number=0; i<nums.length; i++){
+//         if (nums.indexOf(nums[i]) !== i) {
+//             nums.splice(i,1);
+//             i--;
+//         }
+//     }
+//     return nums.length;
+// };
+
 function removeDuplicates(nums: number[]): number {
-    for(let i: number=0; i<nums.length; i++){
-        if (nums.indexOf(nums[i]) !== i) {
-            nums.splice(i,1);
-            i--;
+    if (nums.length === 0) return 0;
+
+    let k = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
         }
     }
-    return nums.length;
-};
+
+    return k;
+}
