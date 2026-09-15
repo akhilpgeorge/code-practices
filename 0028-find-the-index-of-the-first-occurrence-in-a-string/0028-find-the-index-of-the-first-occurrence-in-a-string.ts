284c2caffd148d8855b22b4f5e757e11cc6c90ae
akhilpgeorge <akhilpgeorge@gmail.com>
2026-09-15 19:55:22 +0530

    Time: 1 ms (55.5%), Space: 54.9 MB (80.92%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,14 +1,11 @@
 function strStr(haystack: string, needle: string): number {
-    let needleLength = needle.length; //3
-    let haystackLength = haystack.length; //9
+    let needleLength = needle.length;
+    let haystackLength = haystack.length;
     if(needleLength > haystackLength) return -1;
     for(let i=0; i<=(haystackLength-needleLength); i++) {
-        console.log(`substring= ${haystack.substring(i,(i+needleLength))}`);
-        console.log(`needle= ${needle}`);
         if(haystack.substring(i,(i+needleLength)) === needle) {
             return i;
         }
     }
-    console.log(`return -1 and exit code`);
     return -1;
 };
