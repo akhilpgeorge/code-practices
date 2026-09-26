b063ec8f0a42aa9f4c273f5897100598b9461ab1
akhilpgeorge <akhilpgeorge@gmail.com>
2026-09-26 20:50:22 +0530

    Time: 7 ms (32.72%), Space: 59.1 MB (48.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,4 +1,14 @@
 function isPalindrome(s: string): boolean {
     const clean = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    return clean === clean.split("").reverse().join("");
+    console.log(`clean - ${clean}`);
+    let left = 0;
+    let right = clean.length - 1;
+    while(left < right) {
+        if (clean[left] !== clean[right]){
+            return false;
+        }
+        left ++;
+        right --;
+    }
+    return true;
 };
